30c40708d6619ae6b3f2873f1b6cd03950179755
S.Oses <85915231+soses0512@users.noreply.github.com>
2025-02-26 17:30:59 -0600

    fixing stuff

--- main.tf
+++ main.tf
@@ -54,43 +54,28 @@ module "alb" {
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
 
-  listeners = {
-    ex-http-https-redirect = {
-      port     = 80
-      protocol = "HTTP"
-      redirect = {
-        port        = "443"
-        protocol    = "HTTPS"
-        status_code = "HTTP_301"
-      }
-    }
-    ex-tcp = {
-      port     = 83
-      protocol = "TCP"
-      forward = {
-        target_group_key = "ex-target"
-      }
-    }
-    # ex-https = {
-    #   port            = 443
-    #   protocol        = "HTTPS"
-    #   certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
-
-    #   forward = {
-    #     target_group_key = "ex-instance"
-    #   }
-    # }
-  }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "h1"
-      protocol         = "HTTP"
-      port             = 80
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      target_id        = aws_instance.blog.id
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
-  }
+  ]
+  
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
 
   tags = {
     Environment = "dev"
